0e79b50cffee76b98769e500175ba3d5f2b2c910
Hokedm <hokedm@dukes.jmu.edu>
2019-09-18 09:47:15 -0400

    added variable addition js file

---
+++ jscript/var-add.js
@@ -0,0 +1,7 @@
+var x = 5;         // assign the value 5 to x
+var y = 2;         // assign the value 2 to y
+var z = x + y;     // assign the value 7 to z (x + y)
+
+console.log('first number is ' + x);
+console.log('Second number is ' + y);
+console.log('Their sum together is ' + z);
